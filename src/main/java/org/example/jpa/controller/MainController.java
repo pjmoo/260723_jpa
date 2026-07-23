23387e577a04136c7a824bf2d69da9694de80e38
pjmoo <00pjmoooo@gmail.com>
2026-07-23 14:06:30 +0900

    메인 화면을 구성한다

---
+++ src/main/java/org/example/jpa/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.example.jpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>JPA</title>
+</head>
+<body>
+<h1>JPA</h1>
+</body>
+</html>
